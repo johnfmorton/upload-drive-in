3220e94bf027bcedd34f59816bbbc903c08da37e
John F Morton <morton@jmx2.com>
2025-04-07 14:13:24 -0400

    Subfolder work

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     {
         return $this->role === 'client';
     }
+
+    public function canLoginWithPassword()
+    {
+        return !$this->isClient();
+    }
 }
