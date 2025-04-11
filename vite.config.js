489a194f547f200db5b2b85946ff5272db5a9e81
John F Morton <morton@jmx2.com>
2025-04-11 17:17:29 -0400

    Work on DDEV build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load .env file
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    https: false,
+    strictPort: true,
+    origin: process.env.VITE_DEV_SERVER_URL,
+    cors: {
+      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+  },
 });
