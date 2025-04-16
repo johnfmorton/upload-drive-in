2f4916a2e2d665c9eb143c4921f8cc7391287dcd
John F Morton <morton@jmx2.com>
2025-04-16 16:53:49 -0400

    recovery codes added

--- resources/views/vendor/laravel-admin-2fa/layouts/guest.blade.php
+++ resources/views/vendor/laravel-admin-2fa/layouts/guest.blade.php
@@ -7,6 +7,13 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        {{-- Inject Brand Color CSS Variable --}}
+        <style>
+            :root {
+                --brand-color: {{ config('branding.color', '#6366F1') }};
+            }
+        </style>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -16,11 +23,11 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            {{-- <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
-            </div>
+            </div> --}}
 
             @yield('content')
         </div>
