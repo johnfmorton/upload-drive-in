9275b1b5ad8bfab731899225152f349a86e49b60
John F Morton <morton@jmx2.com>
2025-04-23 14:45:25 -0400

    allow language overrides

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Merge translation overrides from messages.override.php
+        $baseFile = resource_path('lang/en/messages.php');
+        $overrideFile = resource_path('lang/en/messages.override.php');
+
+        if (file_exists($baseFile)) {
+            $messages = require $baseFile;
+
+            if (file_exists($overrideFile)) {
+                $messages = array_merge($messages, require $overrideFile);
+            }
+
+            app('translator')->addLines($messages, 'en');
+        }
     }
 }
