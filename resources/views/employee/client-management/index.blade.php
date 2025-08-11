c3e6db0365ece6316543e538e97ecfb680e808eb
John F Morton <john@johnfmorton.com>
2025-08-11 10:08:26 -0400

    Removed the drive connection from employee client page

--- resources/views/employee/client-management/index.blade.php
+++ resources/views/employee/client-management/index.blade.php
@@ -8,9 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
-            <!-- Google Drive Connection Section -->
-            <x-dashboard.google-drive-connection :user="auth()->user()" :is-admin="false" />
-
             <!-- Create Client Form Section -->
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <h2 class="text-lg font-medium text-gray-900 mb-4">
